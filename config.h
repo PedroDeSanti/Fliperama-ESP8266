388893fdfc977e8ed349d31225c5197309b9294d
PedroDeSanti <phmartinsanti@gmail.com>
2022-03-30 09:26:29 -0300

    🔧 Add config.h

---
+++ config.h
@@ -0,0 +1,53 @@
+/**
+ * @file config.h
+ *
+ * @author Pedro Henrique Martins de Santi <pedrodesanti@usp.br>
+ *
+ * @brief Configuration and definition for ESP8266 project
+ *
+ * @date 2022-03-30
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+// Buttons from right to left
+#define GPIO_BUTTON_1 16
+#define GPIO_BUTTON_2 5
+#define GPIO_BUTTON_3 4
+#define GPIO_BUTTON_4 14
+
+#define TOPIC_BUTTON_1 "grupo2-bancadaA4/E6"
+#define TOPIC_BUTTON_2 "grupo2-bancadaA4/E5"
+#define TOPIC_BUTTON_3 "grupo2-bancadaA4/E4"
+#define TOPIC_BUTTON_4 "grupo2-bancadaA4/E3"
+
+// LEDs from right to left
+#define GPIO_LED_1 0
+#define GPIO_LED_2 2
+#define GPIO_LED_3 12
+#define GPIO_LED_4 13
+
+#define TOPIC_LED_1 "grupo2-bancadaA4/S3"
+#define TOPIC_LED_2 "grupo2-bancadaA4/S2"
+#define TOPIC_LED_3 "grupo2-bancadaA4/S1"
+#define TOPIC_LED_4 "grupo2-bancadaA4/S0"
+
+// Buzzer
+#define GPIO_BUZZER 15
+
+#define NOTE_DO_1 262
+#define NOTE_RE_1 294
+#define NOTE_MI_1 330
+#define NOTE_FA_1 349
+#define NOTE_SOL_1 392
+#define NOTE_LA_1 400
+#define NOTE_SI_1 494
+#define NOTE_DO_2 262
+#define NOTE_RE_2 294
+#define NOTE_MI_2 330
+#define NOTE_FA_2 349
+#define NOTE_SOL_2 392
+#define NOTE_LA_2 400
+#define NOTE_SI_2 494
+#define NOTE_DO_3 527
